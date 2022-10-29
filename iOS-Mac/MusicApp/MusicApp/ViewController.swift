0e8a4ffa723c6df1f5c0d2316e84d3ac20dc11d3
Logan <meltonlogan@me.com>
2022-10-29 16:25:51 -0500

    EmptyXcodeProject

---
+++ iOS-Mac/MusicApp/MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Logan Melton on 10/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
